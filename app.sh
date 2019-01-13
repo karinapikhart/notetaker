bc9d9f6f416906200746cc860725c5a3dbbe6d7f
Karina <Karina@Karinas-Mac.local>
2019-01-13 13:36:25 -0800

    Getting app.sh working, which I hope to turn into a binary/executable eventually

--- app.sh
+++ app.sh
@@ -1,3 +1,5 @@
 cd "/Users/Karina/Dropbox/Projects/software/Notetaker"
 php -S localhost:8000
-open "http://www.google.com"
+
+# need to do this in a new terminal window because current window is tied up with the php server session...
+open "http://localhost:8000"
